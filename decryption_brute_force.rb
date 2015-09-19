5746a2c5db7eefe2daa8e98ae931ababd96676e5
c650 <cbailey@palmertrinity.org>
2015-09-19 16:58:48 -0400

    fixed a few bugs

--- decryption_brute_force.rb
+++ decryption_brute_force.rb
@@ -4,12 +4,12 @@ $keywords = ["mine","the","if","of","me","you","on","off","first","last","maybe"
 def brute_decrypt(phrase)
 	0.upto(25) do |key|
 	  dec = decrypt(key, phrase)
-	  `echo #{dec} >> ./decryption_results.txt`
+	  `echo #{dec} >> ~/Desktop/decryption_results.txt`
 	  $keywords.each do |keyword|
 	    if dec.include? keyword
 	      puts dec
 	    end
 	  end
 	end
-	puts "Text file has been made in current directory: #{"#{`pwd`}".blue}."
+	puts "Text file has been made in the directory: #{"~/Desktop".blue}."
 end
