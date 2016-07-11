6fe808e66546149d64241d44524041c9567d53a8
c650 <cbailey@palmertrinity.org>
2016-07-11 11:32:00 -0400

    revise filesystem and add cpp

---
+++ cpp/caesar-cipher.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+int main() {
+    int n; std::cin >> n;
+    std::string s; std::cin >> s;
+    int k; std::cin >> k;
+
+    for (auto& c : s) {
+        if (c >= 'a' && c <= 'z') {
+
+            c = ((c - 'a' + k) % 26) + 'a';
+
+        } else if (c >= 'A' && c <= 'Z') {
+
+            c = ((c - 'A' + k) % 26) + 'A';
+
+        }
+    }
+
+    std::cout << s << std::endl;
+    return 0;
+}
