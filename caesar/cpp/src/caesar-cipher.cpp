ee49c8f177f8df366dd1831b6bdabc359991e862
Charles <c650@users.noreply.github.com>
2017-03-08 19:45:26 -0500

    revise cipher a bit

--- caesar/cpp/src/caesar-cipher.cpp
+++ caesar/cpp/src/caesar-cipher.cpp
@@ -3,6 +3,7 @@
 int main() {
     std::string s; std::getline(std::cin, s);
     int k; std::cin >> k;
+	k %= 26; // just in case
 
     std::cout << Caesar::encrypt(s, k) << std::endl;
     return 0;
--- caesar/cpp/src/encrypt.cpp
+++ caesar/cpp/src/encrypt.cpp
@@ -2,22 +2,24 @@
 #include <string>
 #include <iostream>
 
+#include <stdexcept>
+
 namespace Caesar {
 
 	std::string encrypt(const std::string& str, const int& key) {
-	    
+		if (key < 0)
+			throw std::logic_error("Don't use a negative key!");
+
 	    std::string s = str;
 
+		char base;
 	    for (auto& c : s) {
 	        if (c >= 'a' && c <= 'z') {
-
-	            c = ((c - 'a' + key) % 26) + 'a';
-
+				base = 'a';
 	        } else if (c >= 'A' && c <= 'Z') {
-
-	            c = ((c - 'A' + key) % 26) + 'A';
-
+				base = 'A';
 	        }
+			c = ((c - base + key) % 26) + base;
 	    }
 
 	    return s;
